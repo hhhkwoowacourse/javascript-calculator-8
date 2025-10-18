236c201db257a44475542fdcab1442e3f84cb629
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-10-18 17:45:33 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,6 +17,7 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
+  // 정답 케이스
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
@@ -32,8 +33,95 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("다중 커스텀 구분자", async () => {
+    const inputs = ["//@#\\n1@2#3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("16진수 덧셈", async () => {
+    const inputs = ["0x01,2,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자 x와 16진수", async () => {
+    const inputs = ["//x\\n0x01,2,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("마이너스 커스텀 구분자", async () => {
+    const inputs = ["//-\\n1,-2,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  // 예외 테스트
+  test("음수 입력", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("구분자 마무리", async () => {
+    const inputs = ["1,2,3,"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("숫자 커스텀 구분자", async () => {
+    const inputs = ["//1\\n1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("커스텀 구분자 선언 문제", async () => {
+    const inputs = ["//#\n1#2#3"];
     mockQuestions(inputs);
 
     const app = new App();
